699b7aea13a1d5f550074f0fd07844996174d707
MxU <u.maxim@gmx.de>
2015-01-28 21:27:00 +0100

    -[Misc] comment-tree-optics-refinary

--- SOIAstd/src/SO_SysInterface/main.cpp
+++ SOIAstd/src/SO_SysInterface/main.cpp
@@ -19,7 +19,6 @@ int main()
 	IA::Engine_SSt* currentEngine = new IA::Engine_SSt(currentGame);
 
 	std::thread t1 = std::thread(&IA::Engine::Start, &*currentEngine);
-	//currentEngine->MThread.AddLoops(5);
 
 	SOIA::ConsoleService* console = new SOIA::ConsoleService(currentEngine);
 	console->Start();
