a111a70c931c5c4d6c44388f5ce98c5f8fba069f
MeerLin <merlin.brandes@web.de>
2015-07-23 17:35:51 +0200

    -[Environment-Text] Specific catching for characters on console

--- SOIA/Source/Environment/Text/Private/StdDialogue.cpp
+++ SOIA/Source/Environment/Text/Private/StdDialogue.cpp
@@ -5,6 +5,7 @@
 using namespace Environment;
 
 #include <iostream>
+#include <conio.h>
 
 void StdDialogue::Write(const std::string& InText)
 {
@@ -18,5 +19,39 @@ void StdDialogue::WriteLine(const std::string& InText)
 
 void StdDialogue::GetNextLine(std::string& OutText)
 {
-	std::getline(std::cin, OutText);
+	OutText = "";
+	char prev;
+	char ch;
+	while ((ch = _getch()) != 13)
+	{
+		if (ch == '\t')
+		{
+			if (prev == ch)
+			{
+				std::cout << "DoubleTab";
+				prev = 0;
+			}
+			else
+			{
+				std::cout << "Tab";
+			}
+		}
+		else
+		{
+			if (ch == '\b')
+			{
+				_putch(ch);
+				_putch(' ');
+				_putch(ch);
+				OutText = OutText.substr(0, OutText.size() - 1);
+			}
+			else
+			{
+				_putch(ch);
+				OutText += ch;
+			}
+		}
+		prev = ch;
+	}
+	_putch('\n');
 }
